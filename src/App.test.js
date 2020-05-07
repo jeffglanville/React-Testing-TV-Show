26dceb638486bbc57cb8df8010cd41791e391829
Jeff Glanville <jeff.glanville@outlook.com>
2020-05-06 20:01:00 -0500

    add tests to app.test

---
+++ src/App.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import App from "./App";
+
+
+test("App is rendering movie", () => {
+    const { getByText } = render(<App />);
+
+    const text = getByText(/Fetching data.../i);
+    expect(text).toBeInTheDocument();
+});
+
+test('Axios is returning a show', () => {
+    const axios = {
+        get: jest.fn(() => Promise.resolve({data: { episodes: "Season" }})),
+    }
+    const url = 'https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes'
+    render(<App url={ url } />)
+    expect(axios.get).toHaveBeenCalledTimes(0)
+})
+
