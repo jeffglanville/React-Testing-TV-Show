0458635fa176aecc6518a7d977eeb5b73ef33aaf
Jeff Glanville <jeff.glanville@outlook.com>
2020-05-09 11:32:54 -0500

    run test for episodes

--- src/components/Episodes.test.js
+++ src/components/Episodes.test.js
@@ -1,4 +1,10 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render } from '@testing-library/react';
 import Episodes from './Episodes';
 
+test("Episode is rendering properly", () => {
+    const { getByText } = render(<Episodes />);
+
+    const text = getByText("episode")
+    expect(text).toBeInTheDOM();
+})
